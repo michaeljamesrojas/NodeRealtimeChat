d73009a4c48086351f141eb1805e1c4b4c2c5491
Michael James Rojas <michaeljamesrojas123@gmail.com>
2021-02-27 20:04:41 +0800

    Somewhat Good Progress

--- serverSide.js
+++ serverSide.js
@@ -5,25 +5,31 @@ const io = require('socket.io')(http);
 // const fs = require('fs');
 // const files = fs.readdirSync('./');
 
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+//Socket operations
 io.on('connection', (socket) => {
-  // console.log('a user connected (Files:)' + files);
-  // new user().sayHi();
+  console.log('a user connected');
 
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
   
-  socket.on('signLogIn', (name) => {
-    console.log('names');
+  socket.on('signLogIn', (userInfo) => {
+    //TEST:
+    // console.log(userInfo);
+
+    //TODO:
+    //
   });
 
 });
 
-var port = process.env.PORT;
+//Port listening
+const port = process.env.PORT;
 http.listen(port || 3000, () => {
-  console.log('listening on port:' + process.env.PORT);
+  console.log('Server started. Listening on port:' + process.env.PORT);
 });
